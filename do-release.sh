8470918ded5c6939f7c862f8b1b32959431a9ed2
Rohan Ferris <r.ferris@library.uq.edu.au>
2025-06-13 13:32:31 +1000

    update scripts

--- do-release.sh
+++ do-release.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # rferris 2025
 
-ATOM_VER=2.9.1
-REL_VER=1
-REL_STR="${ATOM_VER}-${REL_VER}"
-
-#
-#git commit
-
-# Ask if want to tag and push
-git tag $REL_STR
-
 git push
+git push tag $REL_STR

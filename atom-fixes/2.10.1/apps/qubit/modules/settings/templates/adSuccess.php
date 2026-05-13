c7be304cbaf077d9708d1d3148833e7513e3e70f
Rohan Ferris <r.ferris@library.uq.edu.au>
2026-05-13 14:24:24 +1000

    Bump version: 2.10.1-1

---
+++ atom-fixes/2.10.1/apps/qubit/modules/settings/templates/adSuccess.php
@@ -0,0 +1,57 @@
+<?php decorate_with('layout_2col.php'); ?>
+
+<?php slot('sidebar'); ?>
+
+  <?php echo get_component('settings', 'menu'); ?>
+
+<?php end_slot(); ?>
+
+<?php slot('title'); ?>
+
+  <h1><?php echo __('AD authentication'); ?></h1>
+
+<?php end_slot(); ?>
+
+<?php slot('content'); ?>
+
+  <?php echo $form->renderGlobalErrors(); ?>
+
+  <?php echo $form->renderFormTag(url_for(['module' => 'settings', 'action' => 'ad'])); ?>
+
+    <?php echo $form->renderHiddenFields(); ?>
+
+    <div id="content">
+
+      <fieldset class="collapsible">
+
+        <legend><?php echo __('AD authentication settings'); ?></legend>
+
+        <?php echo $form->ldapHost
+            ->label(__('Host'))
+            ->renderRow(); ?>
+
+        <?php echo $form->ldapPort
+            ->label(__('Port'))
+            ->renderRow(); ?>
+
+        <?php echo $form->ldapBaseDn
+            ->label(__('Base DN'))
+            ->renderRow(); ?>
+
+        <?php echo $form->ldapBindAttribute
+            ->label(__('Bind Lookup Attribute'))
+            ->renderRow(); ?>
+
+      </fieldset>
+
+    </div>
+
+    <section class="actions">
+      <ul>
+        <li><input class="c-btn c-btn-submit" type="submit" value="<?php echo __('Save'); ?>"/></li>
+      </ul>
+    </section>
+
+  </form>
+
+<?php end_slot(); ?>

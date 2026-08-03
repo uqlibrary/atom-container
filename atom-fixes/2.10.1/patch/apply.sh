0ce6d2234d3d00b1b8ff5aeeb056a4d6e9b556c7
Rohan Ferris <r.ferris@library.uq.edu.au>
2026-08-04 07:20:53 +1000

    use patches instead of overwriting files

--- atom-fixes/2.10.1/patch/apply.sh
+++ atom-fixes/2.10.1/patch/apply.sh
@@ -7,3 +7,11 @@ patch -d /atom/src -p 1 < /atom/src/patch/security_yml.patch
 # https://github.com/uqlibrary/atom/commit/3d7e75fde5449ce092a63d4aa416427bdc5a27ae?diff=unified
 echo "Applying oai_newline.patch"
 patch -d /atom/src -p 1 < /atom/src/patch/oai_newline.patch
+
+# Add reusable components to footer
+echo "Applying footer.patch"
+patch -d /atom/src/apps/qubit/templates/ -p 1 < /atom/src/patch/footer.patch
+
+# Fix menu to include AD config
+echo "Fixing LDAP menu item"
+/atom/src/patch/fix_ldap_menu.sh

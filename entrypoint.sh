1cc3b2fb4242810a7f5d2893bc22044400da6a60
Rohan Ferris <r.ferris@library.uq.edu.au>
2026-09-02 10:59:27 +1000

    have entrypoint copy atom src

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,6 @@ set -o nounset
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Clean-ups
-#rm -rf /usr/local/etc/php-fpm.d/*
-rm -rf ${__dir}/cache/*
-
-# Populate configuration files
-php ${__dir}/bootstrap.php $@
-status=$?
-if [ $status -ne 0 ]; then
-    echo "bootstrap.php failed!"
-    exit $status
-fi
-
 case $1 in
     '')
         echo "Usage: (convenience shortcuts)"
@@ -39,6 +27,20 @@ case $1 in
         exec php ${__dir}/symfony jobs:worker
         ;;
     'fpm')
+        # Make src available in copy volume
+        echo "Copying atom src to copy volume."
+        rsync -a --delete --exclude /downloads/* --exclude /uploads/* --exclude /cache/* --exclude /log/* --exclude /config/* --exclude /apps/qubit/config/* /atom/src/ /atom/src-copy/
+        # Clean-ups
+        #rm -rf /usr/local/etc/php-fpm.d/*
+        rm -rf ${__dir}/cache/*
+
+        # Populate configuration files
+        php ${__dir}/bootstrap.php $@
+        status=$?
+        if [ $status -ne 0 ]; then
+            echo "bootstrap.php failed!"
+            exit $status
+        fi
         exec /usr/bin/php-fpm --allow-to-run-as-root
         ;;
 esac
